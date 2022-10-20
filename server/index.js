e1d889eefa3d4d31cfa345190d6b5a28e4a4da1e
hadenhiles <hi@hadenhiles.com>
2022-10-19 21:04:31 -0400

    Change default ports for client/server to 8080 for client and 3000 for server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3030
+const port = 3000
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 const cors = require('cors');
